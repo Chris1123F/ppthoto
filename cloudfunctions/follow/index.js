b1e3f6377b4a9a66f798cc92552707f79efbc2c3
LiangXianwei <1261409294@qq.com>
2019-12-17 21:14:35 +0800

    关注xiugai

--- cloudfunctions/follow/index.js
+++ cloudfunctions/follow/index.js
@@ -2,23 +2,22 @@
 const cloud = require('wx-server-sdk')
 
 cloud.init()
-
+const wxContext = cloud.getWXContext()
+const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  const db = cloud.database()
+  
+  
   console.log("test")
-  db.collection('follow').where({
-    _id: event.id
-  }).update({
+  return await db.collection('follow').doc("b4bbd54c-6c59-4275-b70a-4bac254cfdcf").update({
     data: {
       isFollowFrom: true
     }
   })
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+  // return {
+  //   event,
+  //   openid: wxContext.OPENID,
+  //   appid: wxContext.APPID,
+  //   unionid: wxContext.UNIONID,
+  // }
 }
--- cloudfunctions/unfollow/index.js
+++ cloudfunctions/unfollow/index.js
@@ -2,20 +2,21 @@
 const cloud = require('wx-server-sdk')
 
 cloud.init()
-
+const wxContext = cloud.getWXContext()
+const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  const db = cloud.database()
-  db.collection('follow').doc(event.id).set({
+  return await db.collection('follow').where({
+    _id: event.id
+  }).update({
     data: {
       isFollowFrom: false
     }
   })
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+  // return {
+  //   event,
+  //   openid: wxContext.OPENID,
+  //   appid: wxContext.APPID,
+  //   unionid: wxContext.UNIONID,
+  // }
 }
