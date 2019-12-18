b19a924bd686706d1841e28c66ad9558e452c530
Chris1123F <1040701709@qq.com>
2019-12-18 13:35:05 +0800

    publish

---
+++ cloudfunctions/publish/index.js
@@ -0,0 +1,28 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const wxContext = cloud.getWXContext()
+const db = cloud.database()
+// 云函数入口函数
+exports.main = async (event, context) => {
+  try {
+    var count=db.collection('share').count;
+    return db.collection('share').add({
+      // data 字段表示需新增的 JSON 数据
+      data: {
+        comments: [],
+        content: event.sendData.formData.desc,
+        date: event.sendData.date,
+        icon:'',
+        openID:'',
+        photo:event.sendData.formData.photos,
+        shareID:count+1,
+        star:[],
+        username:''
+      }
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
---
+++ miniprogram/pages/publish/publish.js
@@ -0,0 +1,92 @@
+const app = getApp();
+Page({
+  data: {
+    edit:false,
+    formData:{
+      title:'',
+      desc:'',
+      photos:[]
+    }
+  },
+  onLoad (option) {
+    const self = this;
+    if (option.id){
+      wx.request({
+        url: `${app.host}/photo/api/gallery/modify/${option.id}`,
+        success(res){
+          self.setData({
+            edit : true,
+            formData : {
+              _id: res.data.detail['_id'],
+              title: res.data.detail.title,
+              desc: res.data.detail.desc,
+              photos: res.data.detail.photos
+            }
+          })
+        }
+      })
+    }
+  },
+  titleInput(e){
+    this.setData({
+      'formData.title' : e.detail.value
+    });
+  },
+  descInput(e){
+    this.setData({
+      'formData.desc': e.detail.value
+    });
+  },
+  //添加图片
+  selectPhoto(e){
+    const self = this;
+    wx.chooseImage({
+      count: (9 - self.data.formData.photos.length),
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths;
+        self.setData({
+          'formData.photos':self.data.formData.photos.concat(tempFilePaths)
+        })
+      }
+    })
+  },
+  //删除图片
+  remove(e) {
+    var photos = this.data.formData.photos.slice();
+    photos.splice(e.currentTarget.dataset.id, 1);
+    this.setData({
+      'formData.photos': photos
+    })
+  },
+  //提交表单
+  formSubmit(e){
+    const formData = this.data.formData;
+    if (!formData.title || formData.photos.length < 1){
+      wx.showModal({
+        content: '标题不能为空！照片不能少于1张',
+        showCancel:false,
+      })
+    }else{
+      const sendData = { formData, date:new Date(),icon:'',openID:'',username:''};
+
+      //上传到数据库
+      console.log(sendData)
+      wx.cloud.callFunction({
+        name: "publish",
+        data: {
+          sendData: sendData
+        },
+        success: res => {
+          console.log("publish success")
+        },
+        fail: err => {
+          console.log(err)
+        }
+
+      })
+      
+    }
+  }
+})
