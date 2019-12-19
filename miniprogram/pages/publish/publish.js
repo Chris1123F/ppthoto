4c9fe8c275ca277898d4cc5bc62e5e808de91a67
TrouBleMaKerV <782024062@qq.com>
2019-12-19 15:02:54 +0800

    修改一些小细节

--- miniprogram/pages/publish/publish.js
+++ miniprogram/pages/publish/publish.js
@@ -91,6 +91,26 @@ Page({
   },
   //提交表单
   formSubmit(e){
+    Date.prototype.format = function (fmt) {
+      var o = {
+        "M+": this.getMonth() + 1,                 //月份 
+        "d+": this.getDate(),                    //日 
+        "h+": this.getHours(),                   //小时 
+        "m+": this.getMinutes(),                 //分 
+        "s+": this.getSeconds(),                 //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds()             //毫秒 
+      };
+      if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+      }
+      for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+          fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+      }
+      return fmt;
+    }  
     const formData = this.data.formData;
     if (!formData.title || formData.photos.length < 1){
       wx.showModal({
@@ -98,7 +118,7 @@ Page({
         showCancel:false,
       })
     }else{
-      const sendData = { formData, date:new Date(),icon:this.data.avatarUrl,openID:app.globalData.openid,username:this.data.userInfo.nickName};
+      const sendData = { formData, date: new Date().format("yyyy-MM-dd hh:mm:ss"),icon:this.data.avatarUrl,openID:app.globalData.openid,username:this.data.userInfo.nickName};
 
       //上传到数据库
       console.log(sendData)
